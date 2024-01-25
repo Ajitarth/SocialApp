7ff6b261dba2379847ee9122f518c54429506705
User <user@Users-MacBook-Pro.local>
2024-01-25 15:14:14 +0530

    Added posts app

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from posts.models import Post
 
 def user_login(request):
     if request.method == "POST":
@@ -24,7 +25,9 @@ def user_login(request):
 
 @login_required
 def index(request):
-    return render(request, 'users/index.html')
+    current_user = request.user
+    posts = Post.objects.filter(user= current_user)
+    return render(request, 'users/index.html', {'posts': posts})
 
 
 def register(request):
